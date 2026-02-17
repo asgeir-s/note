8df4f601b36c933b3a845aa6bd0efb061a90ee96
Asgeir <asgeir@cluda.com>
2026-02-17 18:51:06 +0100

    fix: correct installation command to use sudo for apt-get

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -32,6 +32,6 @@ fi
 
 DEB_ABS="$(cd -- "$(dirname -- "$DEB_PATH")" && pwd)/$(basename -- "$DEB_PATH")"
 
-d dev/n do apt-get install -y "$DEB_ABS"
+sudo apt-get install -y "$DEB_ABS"
 
 echo "Installed $(basename -- "$DEB_ABS")"
